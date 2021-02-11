0ee7bf9c05c7e6a5a13aff849dd8bf646ef2f4ef
jibundeyare <contact@jibundeyare.com>
2021-02-12 00:30:28 +0100

    Configuration des routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\FooController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'index'])->name('main.index');
+Route::resource('foo', FooController::class);
